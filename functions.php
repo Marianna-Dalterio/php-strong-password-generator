616928359d6561653568649fdfe1e35349b30793
Marianna-Dalterio <mariannadalterio00@gmail.com>
2026-01-06 22:07:43 +0100

    complete Milestone 3

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
-// <!-- recupero dato, prendo la lunghezza richiesta usando $_GET e controllo che il dato esista, trasformo il valore ricevuto in numero intero -->
-//    $length = isset($_GET["pass_length"]) ? $_GET["pass_length"] : null;
-$length = (int) ($_GET["pass_length"] ?? null); 
+//IN QUESTO FILE SOLO GLI STRUMENTI, IN INDEX.PHP LA LOGICA DI CONTROLLO. 
 
 // <!-- creo un unico "grande calderone" da cui pescare -->
  $lower_case= range('a','z');
@@ -25,19 +23,7 @@ function generatePassword ($length, $all_characters){
     
 }
 
-//eseguo la funzione 
-$final_password = "";
-$error_message = "";
 
-//controllo se l'utente ha interagito, all'inizio pagina pulita, se inserisco un numero genero password altrimenti se premo invio senza numero ottengo messaggio di errore. 
-
-// Questo basta a capire se c'è un numero valido
-    if ($length > 0) {
-    $final_password = generatePassword($length, $all_characters);
-    } else if (isset($_GET["pass_length"])) {
-        // Se è entrato qui, significa che il parametro esiste ma non è > 0
-        $error_message = "Nessun parametro valido inserito.";
-    }
 
 
 
